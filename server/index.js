571aedd20d8872f1325cc3e28f250062a555b728
enginerror <ayaanmurshedkhan@gmail.com>
2025-11-28 21:08:24 +0530

    implement api response and api error

--- server/index.js
+++ server/index.js
@@ -1,20 +1,11 @@
 import dotenv from "dotenv";
-import express from "express";
+import app from './app.js'
 
 dotenv.config({
   path: "./.env",
 });
 
-const app = express();
 const port = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-app.get("/instagram", (req, res) => {
-  res.send("This is an instagram");
-});
 
 app.listen(port, () => {
   console.log(`Server listening on port http://localhost:${port}`);
